ee790ef5670b94a2cdb8e85a860affb9a609a065
Oliver Uvman <oliver.uvman@gmail.com>
2016-10-26 21:26:11 +0800

    Add type declarations
    Imports now give us type info.
    Necessary steps to run are now:
    * npm install -g typings
    * typings install
    * npm start

--- src/startup.ts
+++ src/startup.ts
@@ -1,8 +1,16 @@
+/// <reference path="../typings/index.d.ts" />
+/// <reference path="../zazeninjector.d.ts" />
+
 'use strict'
 
-const co = require('co')
-const Zeninjector = require('zazeninjector')
-const container = new Zeninjector()
+import * as co from 'co';
+
+// This `import x = require` is necessary because the package
+// exports a single class, something not supported by ES6, which
+// TS have therefore invented their own syntax for.
+import Zeninjector = require('zazeninjector');
+
+const container = new Zeninjector();
 
 var boot = function* () {
 	yield container.scan(__dirname + '/**/*.js')
